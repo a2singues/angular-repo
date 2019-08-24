49a124f5d83a850843aaf2c6fc15578da887c313
a2singues <assi.nguessan@free.fr>
2019-08-24 22:41:39 +0200

    Commit initial

---
+++ HelloWorld/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'ng2-pokemon-app',
+//    template: `<h1>Hello, Angular 4 !</h1>`,
+    template: '<h1>Hello, Angular 4 !</h1>' 
+})
+export class AppComponent { }
