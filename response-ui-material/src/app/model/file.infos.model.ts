2dc70f89000c5990360cf9a877503cf06afbdf77
assi.nguessan@free.fr <assi.nguessan@free.fr>
2020-11-25 21:35:54 +0100

    Suppr. tmp response-ui-material

--- response-ui-material/src/app/model/file.infos.model.ts
+++
@@ -1,9 +0,0 @@
-export interface FileInfosModel {
-
-         id : String,
-         name ?: String,
-         url ?: String,
-         selected ?: boolean, //--- true=selected as default
-         status : number, //--- 0: valid, 1: to remove
-         updateDate ?: Date
-     }
--- response-ui-material/src/app/model/userError.model.ts
+++
@@ -1,4 +0,0 @@
-export interface UserErrorModel {
-    status : String,
-    message ?: String
-}
